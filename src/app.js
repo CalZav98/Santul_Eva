83c865b01609157489386b95603a4728b0077352
CalZav98 <ingjcal@outlook.com>
2025-02-22 16:50:39 -0600

    Creacion del servidor de la API

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const config = require('./config');
+
+const usuarios = require('./modules/usuarios/rutas');
+
+const app = express();
+
+// Configuracion 
+app.set('port', config.app.port);
+
+// Rutas
+app.use('/api/usuarios', usuarios)
+
+module.exports = app;
